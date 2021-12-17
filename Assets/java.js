2c90d798345e942b568201cae98e13c66b8f5daa
unknown <maxwittner01@gmail.com>
2021-12-16 23:00:05 -0700

    first commit

---
+++ Assets/java.js
@@ -0,0 +1,80 @@
+const timeDisplayEl = $('#time-display')
+const NineAM= $('#9am')
+const TenAM= $('#10am')
+const ElevenAM= $('#11am')
+const TwelvePM= $('#12pm')
+const OnePM= $('#13pm')
+const TwoPM= $('#14pm')
+const ThreePM= $('#15pm')
+const FourPM= $('#16pm')
+const FivePM= $('#17pm')
+
+var rightNow = moment().format('H')
+var hourSpan;
+var User
+
+function displayTime() {
+    var rightNowTitle = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNowTitle);
+  }
+  
+  setInterval(displayTime, 1000);
+
+
+function backgroundHour (){
+  $(".Text").each(function () {
+    var TimeColor = parseInt($(this).attr("id"))
+    rightNow = parseInt(rightNow)
+    // console.log(TimeColor)
+    // console.log("hour ", rightNow)
+    // console.log(this)
+    if (rightNow > TimeColor){
+    $(this).addClass('past')
+    } else if (rightNow = TimeColor) {
+      $(this).addClass('present')
+    } else {
+      $(this).addClass('future')
+    }
+    
+  })
+}
+
+
+
+  function initPage(){
+    console.log("Hour ", rightNow)
+  var initNine = JSON.parse(localStorage.getItem('9:00 am'))
+  NineAM.val(initNine)
+  var initTen = JSON.parse(localStorage.getItem('10:00 am'))
+  TenAM.val(initTen)
+  var initEleven = JSON.parse(localStorage.getItem('11:00 am'))
+  ElevenAM.val(initEleven)
+  var initTwelve = JSON.parse(localStorage.getItem('12:00 pm'))
+  TwelvePM.val(initTwelve)
+  var initOne = JSON.parse(localStorage.getItem('1:00 pm'))
+  OnePM.val(initOne)
+  var initTwo = JSON.parse(localStorage.getItem('2:00 pm'))
+  TwoPM.val(initTwo)
+  var initThree = JSON.parse(localStorage.getItem('3:00 pm'))
+  ThreePM.val(initThree)
+  var initFour = JSON.parse(localStorage.getItem('4:00 pm'))
+  FourPM.val(initFour)
+  var initFive = JSON.parse(localStorage.getItem('5:00 pm'))
+  FivePM.val(initFive)
+   
+}
+// console.log(initPage)
+
+$(document).ready(function () {
+  backgroundHour()
+  initPage ()
+$('.saveBtn').on('click', function () {
+  User = $(this).siblings(".Text").val().trim()
+  console.log(User)
+  hourSpan = $(this).siblings(".time-block").text()
+  console.log(hourSpan)
+  localStorage.setItem(hourSpan, JSON.stringify(User))
+})
+})
+
+
---
+++ README.md
@@ -0,0 +1,23 @@
+# 05 Third-Party APIs: Work Day Scheduler
+
+## Your Task
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+# Homework 5
+this is a daily calender that give you the date and time. This daily planner change colors depending on what hour you are on (green = future, red = present grey = past). This planner will also save all text you put in the textbox with the save button
+
+
+!()[Assets\img\Work-Day-Scheduler.png]
+
+
